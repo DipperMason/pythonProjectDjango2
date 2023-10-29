75429a4a1f43d2b793a2fb3120d5964b2d5d2dc0
DipperMason <amirgaleev2005@yandex.ru>
2023-10-29 22:39:06 +0300

    first commit

--- lyceum/about/tests.py
+++ lyceum/about/tests.py
@@ -1,3 +1,9 @@
+import django.test
 from django.test import TestCase
 
+
 # Create your tests here.
+class StaticURLTests(TestCase):
+    def test_about_endpoint(self):
+        response = django.test.Client().get('/about')
+        self.assertEqual(response.status_code, 200)
